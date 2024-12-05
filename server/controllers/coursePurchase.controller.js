380f1291985ac1272f48ae1ec5e74f96c439eb54
lam-nguyen-impact360 <lam.nguyen@impact360design.com>
2024-12-05 14:24:32 +0700

    fix

--- server/controllers/coursePurchase.controller.js
+++ server/controllers/coursePurchase.controller.js
@@ -20,7 +20,7 @@ export const createCheckoutSession = async (req, res) => {
       line_items: [
         {
           price_data: {
-            currency: "inr",
+            currency: "usd",
             product_data: {
               name: course.courseTitle,
               images: [course.courseThumbnail],
